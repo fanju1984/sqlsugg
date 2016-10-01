10f740bd3abbd94a98983d7198b7598299aba758
Ju Fan <fanju1984@gmail.com>
2016-10-02 06:46:47 +0800

    Resolved a hard-code problem

--- src/main/java/sqlsugg/config/Config.java
+++ src/main/java/sqlsugg/config/Config.java
@@ -1,11 +1,60 @@
 package sqlsugg.config;
 
+import java.io.*;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+
 public class Config {
 	public Domain domain;
-	public static String dbUser = "sqlsugg";
-	public static String dbPass = "sqlsugg";
+	public static String dbHost = null;
+	public static String dbUser = null;
+	public static String dbPass = null;
 	public static String stopfile = "etc/stopwords.txt";
+	final String dsFilename = "etc/dataset_config.json";
+	
 	public Config (String domainName) {
+		// Step 1: Register Datasets from File 
+		File file = new File (dsFilename);
+		StringBuffer buff = new StringBuffer();
+		try {
+			if (!file.exists()) 
+				throw new Exception ("Dataset registry file is missing");	
+			BufferedReader r = new BufferedReader (
+					new FileReader(file.getAbsolutePath()));
+			String line = r.readLine();
+			while (line != null) {
+				buff.append(line);
+				buff.append("\n");
+				line = r.readLine();
+			}
+			r.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+				
+		String str = buff.toString();
+		
+		JSONArray jdss = new JSONArray (str);
+		for (int i = 0; i < jdss.length(); i ++) { // start to parse command conf-file
+			JSONObject jds = jdss.getJSONObject(i);
+			// command name
+			String dsName = jds.getString("name");
+			if (!dsName.equals(domainName)) continue;
+			// command class
+			dbHost = jds.getString("dbHost");
+			dbUser = jds.getString("dbUser");
+			dbPass = jds.getString("dbPass");
+			
+			domain = new Domain (dsName, 
+					jds.getString("schemaFile"), 
+					jds.getString("dbName"));
+			break;
+		}
+		
+		/*
 		if (domainName.equals("dblp")) {
 		domain = new Domain ("dblp", 
 				"data/dblp/schema.dat", 
@@ -27,7 +76,7 @@ public class Config {
 			domain = new Domain ("dblp_server", 
 					"data/dblp_server/schema.dat", 
 					"sqlsugg_dblp_server");
-		}
+		}*/
 	}
 	
 	public String getDomainName () {
